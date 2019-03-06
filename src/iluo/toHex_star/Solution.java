71b8fbc3ed859af740ded37e2ae76cf99b82888f
ILUO <2323221725@qq.com>
2019-03-06 11:06:23 +0800

    toHex

---
+++ src/iluo/toHex_star/Solution.java
@@ -0,0 +1,18 @@
+package iluo.toHex_star;
+
+public class Solution {
+    public String toHex(int num) {
+        if(num == 0) return "0";
+        String hex = "0123456789abcdef", ans = "";
+        while(num != 0 && ans.length() < 8){
+            ans = hex.charAt(num & 0xf) + ans;
+            num >>= 4;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.toHex(-1));
+    }
+}
