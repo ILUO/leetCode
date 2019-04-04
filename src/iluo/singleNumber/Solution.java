08d4275f01373e9ed58d9a79c5c0df679df017e0
ILUO <2323221725@qq.com>
2019-04-04 14:41:55 +0800

    singleNumber

---
+++ src/iluo/singleNumber/Solution.java
@@ -0,0 +1,11 @@
+package iluo.singleNumber;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int a = 0;
+        for(int num:nums){
+            a = a ^ num;
+        }
+        return a;
+    }
+}
