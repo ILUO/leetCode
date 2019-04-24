2f5d0e3e642cdbf7b0de9f36265182ebc54c1966
ILUO <2323221725@qq.com>
2019-04-24 12:42:36 +0800

    letterCombinations

---
+++ src/iluo/letterCombinations/Solution.java
@@ -0,0 +1,35 @@
+package iluo.letterCombinations;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution {
+    private HashMap<Character,String> table = new HashMap<>();
+    private List<String> res = new ArrayList<>();
+    public List<String> letterCombinations(String digits) {
+        table.put('2',"abc");
+        table.put('3',"def");
+        table.put('4',"ghi");
+        table.put('5',"jkl");
+        table.put('6',"mno");
+        table.put('7',"pqrs");
+        table.put('8',"tuv");
+        table.put('9',"wxyz");
+        if(digits.length() == 0) return res;
+        backTrack(digits,new StringBuffer(),0);
+        return res;
+    }
+
+
+    private void backTrack(String digits,StringBuffer temp,int cnt){
+        if(cnt >= digits.length()){
+            res.add(new String(temp.toString()));
+            return;
+        }
+        for(int i = 0;i < table.get(digits.charAt(cnt)).length();i++){
+            backTrack(digits,temp.append(table.get(digits.charAt(cnt)).charAt(i)),cnt+1);
+            temp.deleteCharAt(temp.length()-1);
+        }
+    }
+}
