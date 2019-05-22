fdf129d1129a081f39dabbc8902894521a892b3c
ILUO <2323221725@qq.com>
2019-05-22 16:43:01 +0800

    canJump

---
+++ src/iluo/canJump_star/Solution.java
@@ -0,0 +1,14 @@
+package iluo.canJump_star;
+
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int curMaxStep = nums[0];
+        for(int i = 1;i < nums.length;i++){
+            if(curMaxStep < i) return false;
+            int temp = i + nums[i];
+            if(temp > curMaxStep) curMaxStep = temp;
+            if(curMaxStep >= nums.length - 1) return true;
+        }
+        return curMaxStep >= nums.length -1;
+    }
+}
