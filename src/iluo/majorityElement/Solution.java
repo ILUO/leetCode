5acdbe38edfae07c24456443ec39b54172c827ea
ILUO <2323221725@qq.com>
2019-07-08 23:58:23 +0800

    majorityElement

---
+++ src/iluo/majorityElement/Solution.java
@@ -0,0 +1,16 @@
+package iluo.majorityElement;
+
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int res = nums[0];
+        int count = 1;
+        for(int i = 1;i < nums.length;i++){
+            if(count == 0){
+                res = nums[i];
+                count = 1;
+            }else if(res == nums[i]) count++;
+            else count--;
+        }
+        return res;
+    }
+}
